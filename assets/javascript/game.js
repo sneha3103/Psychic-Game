4aaa9deb167825d941b1963f14b734b885839fbc
Sneha Dama <snehadama@Snehas-Air.domain_not_set.invalid>
2018-02-14 00:39:03 -0600

    I added an array of incorrect letters, as well as added a few if functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var winsCount = 0;
 var lossesCount =0;
 var guessesLeft = 9;
 
-//This is what the user picks
+//This is what the user picks/guesses
 var userInput = "";
 
 //allows the computer to select a random letter
@@ -18,36 +18,69 @@ var computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.leng
 
  console.log("Wins: " + winsCount + "Losses: " + lossesCount + "Guesses Left: " + guessesLeft + "Your Guesses so far: " + guessedLetters + "Computer Choice: " + computerChoice);
 
-//Functions 
 
-//When key is released, this function runs. 
- document.onkeyup = function(event) {
-     var userInput = event.key;
+ //FUNCTIONS
 
-// When user presses a key, it should populate in the html site. 
+
+
+//This array holds the incorrect letters that the user has been guessing.
+ var incorrectLetter = [];
+
+ // When user presses a key, it should populate in the html site. 
  document.onkeyup = function(event) {
         var letterPicked = String.fromCharCode(event.keyCode).toLowerCase(); 
-        document.getElementById("userguesses").innerHTML= "Your Guesses so far: " + userInput;
-
-    }
-    //If user input equals to computer choice, then increase the win count by 1. Otherwise, Your guesses left decreases by 1. 
-     if (userInput === computerChoice) {
-         winsCount ++;
-     } else {
-         guessesLeft --;
-     }
     
-     //If guesses left = 0, then the Losses will increase by 1. 
-     if (guessesLeft === 0){
-         lossesCount++
-     }
- }
+         //push the letterpicked into array of incorrect letters 
+        incorrectLetter.push(letterPicked);
+        console.log(incorrectLetter);
+
+        document.getElementById("userguesses").innerHTML= "Your Guesses so far: " + incorrectLetter.join(' , ');
+
+        if (guessedLetters.indexOf(userInput) < 0 && letterChoices.indexOf(userInput)  >= 0) {
+           guessedLetters[guessedLetters.length]=userGuess;
+           guessesLeft--;
+        }
+  
 
 
- //This gets my element from my html and adds the java functions to it
- document.getElementById("wins1").innerHTML = "Wins: " + winsCount;
- document.getElementById("losses1").innerHTML = "Losses " + lossesCount;
- document.getElementById("guesses1").innerHTML=  "Guesses Left: " + guessesLeft;
+    
+        
+        //If user input equals to computer choice, then increase the win count by 1. Then, the number of remaining guesses left (guessesLeft) gets reset as well as the letters that the user has already guessed(guessedLetters). The computer will then pick another letter. 
 
+         if (userInput == computerChoice) {
+             winsCount ++;
+            console.log("Yay! You won!");
+            guessedLetters = [];
+            guessesLeft = 9;
+            computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+            console.log("Wins: " + winsCount + "Losses: " + lossesCount + "Guesses Left: " + guessesLeft + "Your Guesses so far: " + guessedLetters + "Computer Choice: " + computerChoice);
+
+        } 
+
+        
+
+          
+        //If guesses left = 0, then the Losses will increase by 1. Then, the number of remaining guesses left (guessesLeft) gets reset as well as the letters that the user has already guessed(guessedLetters). And then the computer will begin to pick another letter. 
+
+         if (guessesLeft == 0){
+            lossesCount++;
+            console.log("You lost :( Try again!");
+            guessedLetters = [];
+            guessesLeft = 9;
+            computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+            console.log("Wins: " + winsCount + "Losses: " + lossesCount + "Guesses Left: " + guessesLeft + "Your Guesses so far: " + guessedLetters + "Computer Choice: " + computerChoice);
+
+        }
  
 
+
+     //This gets my element from my html and adds the java functions to it
+     document.getElementById("wins1").innerHTML = "Wins: " + winsCount;
+     document.getElementById("losses1").innerHTML = "Losses: " + lossesCount;
+     document.getElementById("guesses1").innerHTML=  "Guesses Left: " + guessesLeft;
+
+         
+         document.querySelector("#wins1").innerHTML = "Wins: " + winsCount;
+
+ 
+}
